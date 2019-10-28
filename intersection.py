ccf5490a5f40022b00ea22a97323646ad359ed03
Isaac Neal <isaacneal98@gmail.com>
2019-10-28 20:44:02 +0000

    improved file structure

--- intersection.py
+++
@@ -1,5 +0,0 @@
-class Intersection:
-    def __init__(self,ray,object,t):
-        self.ray = ray
-        self.object = object
-        self.t = t
--- ray.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from vectors import *
-
-class Ray:
-    def __init__(self,origin,direction,bounces):
-        self.origin = origin
-        self.direction = normalize(direction)
-        self.bounces = bounces
